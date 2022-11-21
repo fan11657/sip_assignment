230c363f32f33aef5369e817bde85c5cc0157dd1
YuFan <fan11657@hotmail.com>
2022-11-21 12:04:16 +0800

    Accept ip and port via cli arguments

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,22 @@
 #include "iostream"
 
 using namespace std;
-int main()
+int main(int argc, char* argv[])
 {
+	if (argc < 3) {
+		cout << "Missing Arguments" << endl;
+		return -1;
+	}
+
 	SipUser to = {};
 	to.user_name = "ping";
-	to.ip = "0.0.0.0";
-	to.port = 5060;
+	to.ip = argv[1];
+	to.port = stoi(argv[2]);
 
 	SipUser from = {};
 	from.user_name = "ping";
 	from.ip = "0.0.0.0";
-	from.port = 5061;
+	from.port = 60707;
 
 	SipUdpClient sip_udp_client(from, to, "sipping");
 	SipRetryClient sip_retry_client((SipClient *)&sip_udp_client, 10);
