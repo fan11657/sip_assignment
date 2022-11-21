b45b4bfb2cd0c43040db15d1d5afd72f3c857758
YuFan <fan11657@hotmail.com>
2022-11-21 13:27:28 +0800

    Accept max retry via cli argument

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 int main(int argc, char* argv[])
 {
-	if (argc < 3) {
+	if (argc < 4) {
 		cout << "Missing Arguments" << endl;
 		return -1;
 	}
@@ -20,8 +20,10 @@ int main(int argc, char* argv[])
 	from.ip = "0.0.0.0";
 	from.port = 60707;
 
+	int max_retry = stoi(argv[3]);
+
 	SipUdpClient sip_udp_client(from, to, "sipping");
-	SipRetryClient sip_retry_client((SipClient *)&sip_udp_client, 10);
+	SipRetryClient sip_retry_client((SipClient *)&sip_udp_client, max_retry);
 	int result = sip_retry_client.send(SipRequest::Method::OPTIONS);
 	cout << result << endl;
 	return 0;
